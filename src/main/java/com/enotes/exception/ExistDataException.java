37f549fe63929636ad5d4d03ec4a2a1d25cf5761
Bapuraya Mane <bsm.mat2022@gmail.com>
2026-01-17 20:10:24 +0530

    existing category implementation

---
+++ src/main/java/com/enotes/exception/ExistDataException.java
@@ -0,0 +1,11 @@
+package com.enotes.exception;
+
+public class ExistDataException extends RuntimeException{
+
+	public ExistDataException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
--- src/main/java/com/enotes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enotes/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -66,6 +67,18 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<?> handleValidationException(ValidationException e)
 	{
 		return new ResponseEntity<>(e.getErrors(), HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(ExistDataException.class)
+	public ResponseEntity<?> handleExistDataException(ExistDataException e)
+	{
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException e)
+	{
+		return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
 	}
 	
 }

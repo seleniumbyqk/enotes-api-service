317299dafba44bcbf0d5e2c17d6095454f37be02
Bapuraya Mane <bsm.mat2022@gmail.com>
2026-01-26 14:30:23 +0530

    user login security implementation

--- src/main/java/com/enotes/controller/AuthController.java
+++ src/main/java/com/enotes/controller/AuthController.java
@@ -3,11 +3,15 @@ package com.enotes.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.enotes.dto.LoginRequest;
+import com.enotes.dto.LoginResponse;
 import com.enotes.dto.UserDto;
 import com.enotes.service.UserService;
 import com.enotes.util.CommonUtil;
@@ -62,5 +66,20 @@ public class AuthController {
 		
 		//If false
 		return CommonUtil.createErrorResponseMessage("Register failed", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest)
+	{
+		
+		LoginResponse loginResponse = userService.login(loginRequest);
+		
+		if(ObjectUtils.isEmpty(loginResponse))
+		{
+			return CommonUtil.createErrorResponseMessage("Invalid credentials", HttpStatus.BAD_REQUEST);
+		}
+		
+		return CommonUtil.createBuildResponse(loginResponse, HttpStatus.OK);
 	}
 }

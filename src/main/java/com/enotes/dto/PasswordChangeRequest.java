1f36045891af471435b29a757e966671796e8d9d
Bapuraya Mane <bsm.mat2022@gmail.com>
2026-01-30 23:48:34 +0530

    user change password implementation

---
+++ src/main/java/com/enotes/dto/PasswordChangeRequest.java
@@ -0,0 +1,79 @@
+package com.enotes.dto;
+
+import tools.jackson.databind.deser.bean.BuilderBasedDeserializer;
+
+public class PasswordChangeRequest {
+
+	
+	private String oldPassword;
+	
+	private String newPassword;
+
+	public PasswordChangeRequest() {
+		super();
+	}
+
+	public PasswordChangeRequest(String oldPassword, String newPassword) {
+		super();
+		this.oldPassword = oldPassword;
+		this.newPassword = newPassword;
+	}
+
+	public String getOldPassword() {
+		return oldPassword;
+	}
+
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+
+	public String getNewPassword() {
+		return newPassword;
+	}
+
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	@Override
+	public String toString() {
+		return "PasswordChangeRequest [oldPassword=" + oldPassword + ", newPassword=" + newPassword + "]";
+	}
+	
+	
+	//private constructor for Builder
+	private PasswordChangeRequest(Builder builder)
+	{
+		this.oldPassword = builder.oldPassword;
+		this.newPassword = builder.newPassword;
+	}
+	
+	public static Builder builder()
+	{
+		return new Builder();
+	}
+	
+	public static class Builder
+	{
+		private String oldPassword;
+		
+		private String newPassword;
+		
+		public Builder oldPassword(String oldPassword)
+		{
+			this.oldPassword = oldPassword;
+			return this;
+		}
+		
+		public Builder newPassword(String newPassword)
+		{
+			this.newPassword = newPassword;
+			return this;
+		}
+		
+		public PasswordChangeRequest build()
+		{
+			return new PasswordChangeRequest(this);
+		}
+	}
+}
